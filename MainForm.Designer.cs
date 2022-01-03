840eb1254fc530a392f9da3325201813bbeb4dd9
the-qmark <egormalyukow@gmail.com>
2022-01-03 17:54:13 +0300

    Add mainMenuStrip

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,20 +29,113 @@ namespace DBF_Editor
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьdbfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьВdbfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьВdbfcsvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сервисToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.сервисToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(800, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьdbfToolStripMenuItem,
+            this.сохранитьВdbfToolStripMenuItem,
+            this.сохранитьВdbfcsvToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьdbfToolStripMenuItem
+            // 
+            this.открытьdbfToolStripMenuItem.Name = "открытьdbfToolStripMenuItem";
+            this.открытьdbfToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.открытьdbfToolStripMenuItem.Text = "Открыть [dbf]";
+            // 
+            // сохранитьВdbfToolStripMenuItem
+            // 
+            this.сохранитьВdbfToolStripMenuItem.Name = "сохранитьВdbfToolStripMenuItem";
+            this.сохранитьВdbfToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.сохранитьВdbfToolStripMenuItem.Text = "Сохранить в [dbf]";
+            // 
+            // сохранитьВdbfcsvToolStripMenuItem
+            // 
+            this.сохранитьВdbfcsvToolStripMenuItem.Name = "сохранитьВdbfcsvToolStripMenuItem";
+            this.сохранитьВdbfcsvToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.сохранитьВdbfcsvToolStripMenuItem.Text = "Сохранить в [dbf + csv]";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // сервисToolStripMenuItem
+            // 
+            this.сервисToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.сервисToolStripMenuItem.Name = "сервисToolStripMenuItem";
+            this.сервисToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.сервисToolStripMenuItem.Text = "Сервис";
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DBF Editor";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьdbfToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьВdbfToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьВdbfcsvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сервисToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
     }
 }
 
